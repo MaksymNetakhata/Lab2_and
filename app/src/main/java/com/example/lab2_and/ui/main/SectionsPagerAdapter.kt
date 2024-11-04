3e102a4e76a133afdf50fff1a5d18f704602049e
MaksymNetakhata <netakhata.maksym1121@vu.cdu.edu.ua>
2024-11-04 23:58:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab2_and/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.lab2_and.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.lab2_and.R
+import com.example.lab2_and.ui.personal.PersonalInfoFragment
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> PlaceholderFragment.newInstance(1)
+            1 -> PlaceholderFragment.newInstance(2)
+            2 -> PersonalInfoFragment()
+            else -> PlaceholderFragment.newInstance(1)
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 3
+    }
+}
